2979ec40f5d3a968cb2657106df9515baa53877b
eltayeb <gaaliii@yahoo.com>
2020-11-28 14:24:18 +0100

    write-eventlistener

--- client/app/init.js
+++ client/app/init.js
@@ -1,3 +1,8 @@
 import { getstudent } from '../handlers/getstudent.js'
+import { getClassesByModuleName } from '../handlers/getClasses.js';
+
 
 document.getElementById('submit').addEventListener('click',getstudent)
+
+// get classes by module name
+document.querySelector('#getClasses').addEventListener('click', getClassesByModuleName);
