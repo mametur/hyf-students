4c51c7b7a539eeea1c78d365481747df6549faba
eltayeb <gaaliii@yahoo.com>
2020-11-28 14:23:16 +0100

    add-reference-to-route

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
+const modules = require('./modules');
 
 router.get('/', (req, res) => {
 	res.send('hello from API');
 });
 
 router.use('/students', require('./students'));
+
+router.use('/modules', modules);
 
 module.exports = router;
